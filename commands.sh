f7ab114477dcc8157aed27940549e812dd18da96
Leon Cozzi <leoncozzi@gmail.com>
2026-03-13 20:22:53 +0000

    updated instructions in commands.sh

--- commands.sh
+++ commands.sh
@@ -1,3 +1,8 @@
+#terminal 1
+litellm --config config.yaml --detailed_debug
+
+#terminal 2
+
 curl -X POST \
   -H "Authorization: Bearer $(gcloud auth print-access-token)" \
   -H "Content-Type: application/json; charset=utf-8" \
@@ -13,3 +18,28 @@ curl -X POST \
     "stream": true
   }' \
   "https://aiplatform.googleapis.com/v1/projects/duetai-399118/locations/global/publishers/anthropic/models/claude-sonnet-4-6:streamRawPredict"
+
+
+  curl -X POST http://localhost:4000/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer sk-1234" \
+  -d '{
+    "model": "claude-sonnet",
+    "messages": [
+      {"role": "user", "content": "Hello! How are you?"}
+    ]
+  }'
+
+curl -X POST http://localhost:4000/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer sk-1234" \
+  -d '{
+    "model": "claude-sonnet",
+    "messages": [
+      {"role": "user", "content": "My CREDIT_CARD_NUMBER is 1234567890123456"}
+    ]
+  }'
+
+#terminal 3
+
+bash test_loop.sh
